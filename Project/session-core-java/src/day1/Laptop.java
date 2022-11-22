55808d97057cc9516f47131e4a3da159e4925c34
aprantosh <aprantosh@gmail.com>
2022-11-22 09:51:23 -0600

    new state2 update

---
+++ Project/session-core-java/src/day1/Laptop.java
@@ -0,0 +1,16 @@
+package day1;
+
+public class Laptop {
+
+    //state
+    //brand
+    //Color
+    //weight
+    //storage
+    //RAM size
+    //display length
+    //Price
+    //Display type
+    //GraphicsCard Type
+    //Intel Processor Type
+}
